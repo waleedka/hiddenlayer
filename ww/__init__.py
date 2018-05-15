6d6a5ada2f0539c88c3a0b1eb7bf2ebaff49ea0d
Waleed Abdulla <waleed.abdulla@gmail.com>
2018-05-14 22:50:36 -0700

    Plots and functions to show tensors and images

--- ww/__init__.py
+++ ww/__init__.py
@@ -1,3 +1,6 @@
 # Import methods to expose in the library
 from .pytorch import build_pytorch_graph
 from .common import draw_graph
+from .common import show
+from .common import show_images
+from .common import Watcher
