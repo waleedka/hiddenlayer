af41b97ff36f2001ee47b31a9356cf00d53f079d
Phil Ferriere <pferriere@hotmail.com>
2018-06-04 14:38:49 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name = 'weightwatcher',
+    packages = ['ww'],
+    version = '0.1',
+    license="MIT",
+    description = 'A light-weight alternative to TensorBoard for smaller deep learning projects',
+    author = 'Waleed Abdulla <waleed.abdulla@gmail.com>, Phil Ferriere <pferriere@hotmail.com>',
+    url = 'https://github.com/waleedka/weightwatcher',
+    classifiers = [
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Visualization',
+
+    # Pick your license as you wish (should match "license" above)
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3.5',
+    ],
+)
