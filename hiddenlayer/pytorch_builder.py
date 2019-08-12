c84b81abeb459ee8268c3f20baf6568801d500c5
Joseph Curtis <jccurtis@lbl.gov>
2019-08-12 16:59:39 -0700

    Change pytorch_builder to use unique() method
    To support older `torch` versions.

--- hiddenlayer/pytorch_builder.py
+++ hiddenlayer/pytorch_builder.py
@@ -42,7 +42,7 @@ def pytorch_id(node):
     """Returns a unique ID for a node."""
     # After ONNX simplification, the scopeName is not unique anymore
     # so append node outputs to guarantee uniqueness
-    return node.scopeName() + "/outputs/" + "/".join([o.debugName() for o in node.outputs()])
+    return node.scopeName() + "/outputs/" + "/".join(["{}".format(o.unique()) for o in node.outputs()])
 
 
 def get_shape(torch_node):
